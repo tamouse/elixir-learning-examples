588363df5b859bc23354f28fee6c7608462b4072
Tamara Temple <tamouse@gmail.com>
2017-04-17 19:58:29 -0500

    2017-04-17T19:58:29

---
+++ intro/hello.exs
@@ -0,0 +1,6 @@
+# An example from the book "Programming Elixir 1.2",
+
+# Let’s write the classic first program. Go to a working directory and create a file called hello.exs.
+# intro/hello.exs
+
+IO.puts "Hello, World!"
